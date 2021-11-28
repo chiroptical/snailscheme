406e9b6c313439b9ab68afa93772c06ed546a8b3
Barry Moore <chiroptical@gmail.com>
2021-11-28 16:00:29 -0500

    Minor naming updates, remove vector

--- src/Snail/Types.hs
+++ src/Snail/Types.hs
@@ -4,37 +4,29 @@ import Data.Text (Text)
 import Data.Text qualified as T
 import Data.Text.Display (Display (..), display)
 import Data.Text.Lazy.Builder qualified as B
-import Data.Vector (Vector (..))
-import Data.Vector qualified as V
 
-data AST
+data Expression
   = Nil
-  | Expression Text
+  | Symbol Text
   | Boolean Bool
   | Number Integer
   | String Text
-  | Quote AST
-  | List (Vector AST)
-  | Lambda (Vector Text) AST
+  | Quote Expression
+  | List [Expression]
+  | Lambda [Text] Expression
   deriving (Eq)
 
-instance Display AST where
+instance Display Expression where
   displayBuilder = \case
     Nil -> "Nil"
-    Expression expr -> B.fromText expr
+    Symbol expr -> B.fromText expr
     Boolean True -> "#t"
     Boolean False -> "#f"
     Number int -> displayBuilder int
     String str -> "\"" <> B.fromText str <> "\""
     Quote ast -> "'" <> displayBuilder ast
-    List contents -> "(" <> B.fromText (unwordVec (display <$> contents)) <> ")"
-    Lambda contents ast -> "<lambda (" <> B.fromText (unwordVec (display <$> contents)) <> ") (" <> displayBuilder ast <> ")>"
+    List exprs -> "(" <> B.fromText (T.unwords (display <$> exprs)) <> ")"
+    Lambda exprs ast -> "<lambda (" <> B.fromText (T.unwords (display <$> exprs)) <> ") (" <> displayBuilder ast <> ")>"
 
-unwordVec :: Vector Text -> Text
-unwordVec vector = V.foldr1' (<>) $ intercalateVec " " vector
-
-intercalateVec :: Text -> Vector Text -> Vector Text
-intercalateVec separator vector =
-  if V.null vector
-    then vector
-    else V.tail $ V.concatMap (\word -> V.fromList [separator, word]) vector
+instance Show Expression where
+  show = T.unpack . display

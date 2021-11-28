9cbe5cbe924e54938e733cb99b1c00062b1430fb
Barry Moore <chiroptical@gmail.com>
2021-11-28 15:50:38 -0500

    Add fourmolu.yaml and format

--- src/Snail/Types.hs
+++ src/Snail/Types.hs
@@ -8,33 +8,33 @@ import Data.Vector (Vector (..))
 import Data.Vector qualified as V
 
 data AST
-    = Nil
-    | Expression Text
-    | Boolean Bool
-    | Number Integer
-    | String Text
-    | Quote AST
-    | List (Vector AST)
-    | Lambda (Vector Text) AST
-    deriving (Eq)
+  = Nil
+  | Expression Text
+  | Boolean Bool
+  | Number Integer
+  | String Text
+  | Quote AST
+  | List (Vector AST)
+  | Lambda (Vector Text) AST
+  deriving (Eq)
 
 instance Display AST where
-    displayBuilder = \case
-        Nil -> "Nil"
-        Expression expr -> B.fromText expr
-        Boolean True -> "#t"
-        Boolean False -> "#f"
-        Number int -> displayBuilder int
-        String str -> "\"" <> B.fromText str <> "\""
-        Quote ast -> "'" <> displayBuilder ast
-        List contents -> "(" <> B.fromText (unwordVec (display <$> contents)) <> ")"
-        Lambda contents ast -> "<lambda (" <> B.fromText (unwordVec (display <$> contents)) <> ") (" <> displayBuilder ast <> ")>"
+  displayBuilder = \case
+    Nil -> "Nil"
+    Expression expr -> B.fromText expr
+    Boolean True -> "#t"
+    Boolean False -> "#f"
+    Number int -> displayBuilder int
+    String str -> "\"" <> B.fromText str <> "\""
+    Quote ast -> "'" <> displayBuilder ast
+    List contents -> "(" <> B.fromText (unwordVec (display <$> contents)) <> ")"
+    Lambda contents ast -> "<lambda (" <> B.fromText (unwordVec (display <$> contents)) <> ") (" <> displayBuilder ast <> ")>"
 
 unwordVec :: Vector Text -> Text
 unwordVec vector = V.foldr1' (<>) $ intercalateVec " " vector
 
 intercalateVec :: Text -> Vector Text -> Vector Text
 intercalateVec separator vector =
-    if V.null vector
-        then vector
-        else V.tail $ V.concatMap (\word -> V.fromList [separator, word]) vector
+  if V.null vector
+    then vector
+    else V.tail $ V.concatMap (\word -> V.fromList [separator, word]) vector
